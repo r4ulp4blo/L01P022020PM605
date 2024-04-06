dc3af5f774affddecf4f937b33d76f9be359f6fd
r4ulp4blo <fokuto34@gmail.com>
2024-04-06 12:55:17 -0600

    creaccion de controladores y modelos para cada tabla

--- L01P022020PM605/Models/notasDbContext.cs
+++ L01P022020PM605/Models/notasDbContext.cs
@@ -8,5 +8,10 @@ namespace L01P022020PM605.Models
         public notasDbContext(DbContextOptions options) : base(options) 
         {
         }
+
+        public DbSet<facultades> facultades { get; set;}
+        public DbSet<materias> materias { get; set;}
+        public DbSet<departamentos> departamentos { get; set;}
+        public DbSet<alumnos> alumnos { get; set;}
     }
 }
